e91c70485309aa384a45968e3a0e3280ee589d4d
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-03-06 10:06:41 +0100

    -

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getStorage } from "firebase/storage"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkhkyIwRnzRImxqsnySuqQyjoN0Npoy60",
-  authDomain: "chatapp-rct.firebaseapp.com",
-  projectId: "chatapp-rct",
-  storageBucket: "chatapp-rct.appspot.com",
-  messagingSenderId: "163853019670",
-  appId: "1:163853019670:web:a8df4a15a7e4e18153a337",
-  measurementId: "G-06NQ6DJ79R"
+  // your config
 };
 
 // Initialize Firebase
